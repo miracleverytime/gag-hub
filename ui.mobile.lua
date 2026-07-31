4d6ea00db2cda0d95120a265dcebea1f0e918e1b
miracleverytime <faizrizqullah2@gmail.com>
2026-07-31 15:09:53 +0700

    debug(mobile): testing update

--- ui.mobile.lua
+++ ui.mobile.lua
@@ -10,7 +10,7 @@
 -- ======================================================================
 
 return function(ctx)
-    local BUILD_TAG         = "MOBILE-DRAG-15"
+    local BUILD_TAG         = "MOBILE-DRAG-16"
     local Colors             = ctx.Colors
     local States             = ctx.States
     local playerGui          = ctx.playerGui
