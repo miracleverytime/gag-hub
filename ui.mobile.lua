7a08699f92522ce0ac396ebb981a0275a34e24a8
miracleverytime <faizrizqullah2@gmail.com>
2026-08-02 10:08:48 +0700

    debug(mobile): testing update

--- ui.mobile.lua
+++ ui.mobile.lua
@@ -10,7 +10,7 @@
 -- ======================================================================
 
 return function(ctx)
-    local BUILD_TAG         = "B-27"
+    local BUILD_TAG         = "B-28"
     local Colors             = ctx.Colors
     local States             = ctx.States
     local playerGui          = ctx.playerGui
